66d043bbbc9244abdc3c38e28413b8a8bc9c4538
fcs <fredericcazesulfourt@yahoo.fr>
2020-04-11 16:09:29 +0200

    Udemy lesson for calculator modified

--- Udemy_Calculator/MainWindow.xaml.cs
+++ Udemy_Calculator/MainWindow.xaml.cs
@@ -17,87 +17,26 @@ namespace Udemy_Calculator
             InitializeComponent();
         }
 
-        private string mFormula = string.Empty;
-        private string mOperand = "+-*/";
-        private string mNumbers = "123456789";
-
-        private void CleanFormula()
-        {
-            mFormula = "0";
-            formulaLabel.Content = mFormula;
-        }
-
-        private void FormulaLabelDisplay()
-        {
-            formulaLabel.Content = mFormula.Replace(',', '.');
-            if(mFormula.FirstOrDefault() == '0' && mFormula[1] != '.')
-            {
-                formulaLabel.Content = mFormula.Remove(0, 1);
-            }
-        }
-
-        private void MakeFormula(string pNum)
-        {
-            // Règle 1 si mFormula == 0 et pNum = {1-9} => remplacer le 0 par le chiffre
-            if(mFormula == "0" && mNumbers.Contains(pNum))
-                mFormula = pNum;
-            // Règle 2 si Formula == 0 et pNUm = . et si pas d'autre . => laisser le 0 et ajouter le .
-            else if (mFormula == "0" && pNum == "." && !mFormula.Contains("."))
-                mFormula = $"{mFormula}{pNum}";
-            // Règle 3 si Formula == 0 et pNum = *+-/ => laisser le 0 et ajouter le symbole
-            else if (mFormula == "0" && mOperand.Contains(pNum))
-                mFormula = $"{mFormula}{pNum}";
-            // Règle 4 si mFormula != 0 ajouter le chiffre
-            else
-                mFormula = $"{mFormula}{pNum}";
-        }
-
-        private void ResultLabelDisplay(string pNum)
-        {
-            resultLabel.Content = (resultLabel.Content.ToString() == "0" && pNum != ".")
-                || mOperand.Contains(mFormula.LastOrDefault()) ? resultLabel.Content = pNum : resultLabel.Content += pNum;
-
-            resultLabel.Content = resultLabel.Content.ToString().Replace(',', '.');
-
-        }
-
-        private void ComputeNumber(string pNum)
-        {
-            ResultLabelDisplay(pNum);
-            MakeFormula(pNum);
-            FormulaLabelDisplay();
-        }
+        private double mLastNumber;
+        private SelectedOperator mSelectedOperator;
 
         private void numberButton_Click(object sender, RoutedEventArgs e)
         {
             string lNumber = (e.Source as Button).Content.ToString();
-            ComputeNumber(lNumber);
+            resultLabel.Content = lNumber;
         }
 
         private void pointButton_Click(object sender, RoutedEventArgs e)
         {
             if (!resultLabel.Content.ToString().Contains('.'))
             {
-                ComputeNumber(".");
+
             }
         }
 
         private void aCButton_Click(object sender, RoutedEventArgs e)
         {
             resultLabel.Content = "0";
-            CleanFormula();
-        }
-
-        private bool StringToDouble(string pContent, out double pNum)
-        {
-            //pContent = pContent.Replace(',', '.');
-            bool lConverted = double.TryParse(pContent, NumberStyles.Number, CultureInfo.InvariantCulture, out pNum);
-            return lConverted;
-        }
-
-        private void RemoveLastNum(string pNum, Action<string> pAction)
-        {
-            pAction(pNum);
         }
 
         private double TransformCoef(string pButtonPressed)
@@ -116,59 +55,92 @@ namespace Udemy_Calculator
 
         private void transformNumberButton_Click(object sender, RoutedEventArgs e)
         {
-            double lTransformCoef = TransformCoef((e.Source as Button).Content.ToString());       
-            double lNum;
-            resultLabel.Content = resultLabel.Content.ToString().Replace(',', '.');
-            if (StringToDouble(resultLabel.Content.ToString(), out lNum))
+            double lTransformCoef = TransformCoef((e.Source as Button).Content.ToString());
+            double lResult;
+
+            if(double.TryParse(resultLabel.Content.ToString(), out lResult))
             {
-                RemoveLastNum(lNum.ToString(), (string pNumStr) =>
-                {
-                    mFormula = mFormula.Remove(mFormula.Length - pNumStr.Length);
-                });
-
-                RemoveLastNum(lNum.ToString(), (string pNumStr) =>
-                {
-                    resultLabel.Content = resultLabel.Content.ToString().Remove(resultLabel.Content.ToString().Length - pNumStr.Length);
-                });
-
-                lNum *= lTransformCoef;
-                string lResultStr = lNum.ToString().Replace(',', '.');
-                ComputeNumber(lResultStr);
-            }
-        }
-
-        private void ComputeOperator(string pOp)
-        {
-            string lResultLabel = resultLabel.Content.ToString();
-
-            if (lResultLabel != string.Empty || !mOperand.Contains(lResultLabel.Last()) || !mOperand.Contains(mFormula.Last()))
-            {
-                MakeFormula(pOp);
+                resultLabel.Content = lResult * lTransformCoef;
             }
         }
 
         private void operatorButton_Click(object sender, RoutedEventArgs e)
         {
-            string lOperator = (e.Source as Button).Content.ToString();
-            lOperator = lOperator == "X" ? "*" : lOperator;
-            ComputeOperator(lOperator);
+            if(double.TryParse(resultLabel.Content.ToString(), out mLastNumber))
+            {
+                resultLabel.Content = (e.Source as Button).Content.ToString();
+            }
+
+            if(sender == plusButton)
+            {
+                mSelectedOperator = SelectedOperator.Addition;
+            }
+            else if(sender == minusButton)
+            {
+                mSelectedOperator = SelectedOperator.Substraction;
+            }
+            else if(sender == multiplyButton)
+            {
+                mSelectedOperator = SelectedOperator.Multiplication;
+            }
+            else if(sender == divideButton)
+            {
+                mSelectedOperator = SelectedOperator.Division;
+            }          
         }
 
         private void equalButton_Click(object sender, RoutedEventArgs e)
         {
-            if (mFormula != string.Empty && !mOperand.Contains(mFormula.Last()))
+            double lNewNumber;
+            if(double.TryParse(resultLabel.Content.ToString(), out lNewNumber))
             {
-                resultLabel.Content = EvaluateExpression();
-                CleanFormula();
-                string lResultStr = resultLabel.Content.ToString().Replace(',', '.');
-                MakeFormula(lResultStr);
-                FormulaLabelDisplay();
+                switch (mSelectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                       resultLabel.Content = SimpleMath.Add(mLastNumber, lNewNumber);
+                        break;
+                    case SelectedOperator.Substraction:
+                        resultLabel.Content = SimpleMath.Sub(mLastNumber, lNewNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        resultLabel.Content = SimpleMath.Mul(mLastNumber, lNewNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        resultLabel.Content = SimpleMath.Div(mLastNumber, lNewNumber);
+                        break;
+                }
             }
         }
+    }
 
-        private double EvaluateExpression()
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division
+    }
+
+    public static class SimpleMath
+    {
+        public static double Add(double p1, double p2)
         {
-            return Convert.ToDouble(new DataTable().Compute(mFormula.Replace(',', '.'), null));
+            return p1 + p2;
+        }
+
+        public static double Sub(double p1, double p2)
+        {
+            return p1 - p2;
+        }
+
+        public static double Mul(double p1, double p2)
+        {
+            return p1 * p2;
+        }
+
+        public static double Div(double p1, double p2)
+        {
+            return p1 / p2;
         }
     }
 }
