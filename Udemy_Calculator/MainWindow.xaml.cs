22c35b1113f411a5e498995d9c0b43e48023698b
fcs <fredericcazesulfourt@yahoo.fr>
2020-04-25 00:50:59 +0200

    Nouvelle organisation du UserControl Standard + Enumération des Modes + SelectionLabl determiné dynamiquement suivant le choix du mode

--- Udemy_Calculator/MainWindow.xaml.cs
+++ Udemy_Calculator/MainWindow.xaml.cs
@@ -14,202 +14,136 @@ using System.Windows.Threading;
 
 namespace Udemy_Calculator
 {
+    public enum CalculatorMode
+    {
+        /// <summary>
+        /// Main modes
+        /// </summary>
+        Standard,
+        Scientific,
+        Binary,
+        Calendar,
+
+        /// <summary>
+        /// Conversions modes
+        /// </summary>
+        Area,
+        Electronic,
+        Energy,
+        Flow,
+        Force,
+        Length,
+        Power,
+        Pressure,
+        Speed,
+        Temperature,
+        Time,
+        Volume,
+        Weight,
+        Data,
+        Angle,
+        Currency,
+
+        /// <summary>
+        /// Option mode for paraméters
+        /// </summary>
+        Options
+    };
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private decimal mLastNumber;
-        private SelectedOperator mSelectedOperator = SelectedOperator.None;
-        private string mSpecialSymbols = "×÷+-";
-        private bool mIsResult = false;
-        private DisplayHistory mDisplayHistory;
-        private static double mHistoryWidth = 200d;
-        private const double mMainWindowWidth = 280d;
-        private const double mMainWindowHeight = 390d;
-        private GridLength mExpandedWidth = new GridLength(1, GridUnitType.Star);
+        private StandardCalculator mStandardCalcultator;
+        private CalculatorMode eCurrentMode;
 
         public MainWindow()
         {
             InitializeComponent();
-            mDisplayHistory = new DisplayHistory();
-        }  
+            mStandardCalcultator = new StandardCalculator();
+            eCurrentMode = CalculatorMode.Standard;
 
-        private void UIHistoryExpander_Expanded(object sender, RoutedEventArgs e)
+            mStandardCalcultator.SetValue(Grid.RowProperty, 1);
+            mStandardCalcultator.SetValue(Grid.ColumnProperty, 1);
+            MainGrid.Children.Add(mStandardCalcultator);
+            UICalculatorSelectionLabel.Content = eCurrentMode.ToString();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.MainWindow.Width = mMainWindowWidth + mHistoryWidth;
-            UIHistoryExpanderColumn.Width = mExpandedWidth;
-
-            if (HistoryGrid != null)
+            string lItemName = (e.Source as MenuItem).Header.ToString();
+            switch (lItemName)
             {
-                HistoryGrid.Visibility = Visibility.Visible;
-                UICleaner.Visibility = Visibility.Visible;
+                case "_Standard":
+                    eCurrentMode = CalculatorMode.Standard;
+                    break;
+                case "_Scientific":
+                    eCurrentMode = CalculatorMode.Scientific;
+                    break;
+                case "_Binary":
+                    eCurrentMode = CalculatorMode.Binary;
+                    break;
+                case "_Calendar":
+                    eCurrentMode = CalculatorMode.Calendar;
+                    break;
+                case "_Area":
+                    eCurrentMode = CalculatorMode.Area;
+                    break;
+                case "_Electronic":
+                    eCurrentMode = CalculatorMode.Electronic;
+                    break;
+                case "_Energy":
+                    eCurrentMode = CalculatorMode.Energy;
+                    break;
+                case "_Flow":
+                    eCurrentMode = CalculatorMode.Flow;
+                    break;
+                case "_Force":
+                    eCurrentMode = CalculatorMode.Force;
+                    break;
+                case "_Length":
+                    eCurrentMode = CalculatorMode.Length;
+                    break;
+                case "_Power":
+                    eCurrentMode = CalculatorMode.Power;
+                    break;
+                case "_Pressure":
+                    eCurrentMode = CalculatorMode.Pressure;
+                    break;
+                case "_Speed":
+                    eCurrentMode = CalculatorMode.Speed;
+                    break;
+                case "_Temperature":
+                    eCurrentMode = CalculatorMode.Temperature;
+                    break;
+                case "_Time":
+                    eCurrentMode = CalculatorMode.Time;
+                    break;
+                case "_Volume":
+                    eCurrentMode = CalculatorMode.Volume;
+                    break;
+                case "_Weight":
+                    eCurrentMode = CalculatorMode.Weight;
+                    break;
+                case "_Data":
+                    eCurrentMode = CalculatorMode.Data;
+                    break;
+                case "_Angle":
+                    eCurrentMode = CalculatorMode.Angle;
+                    break;
+                case "_Currency":
+                    eCurrentMode = CalculatorMode.Currency;
+                    break;
+                case "_Options":
+                    eCurrentMode = CalculatorMode.Options;
+                    break;
+                default:
+                    eCurrentMode = CalculatorMode.Standard;
+                    break;
             }
+
+            UICalculatorSelectionLabel.Content = eCurrentMode.ToString();
         }
-
-        private void UIHistoryExpander_Collapsed(object sender, RoutedEventArgs e)
-        {
-            Application.Current.MainWindow.Width = mMainWindowWidth;
-            mExpandedWidth = UIHistoryExpanderColumn.Width;
-            UIHistoryExpanderColumn.Width = GridLength.Auto;
-
-            if (HistoryGrid != null)
-            {
-                HistoryGrid.Visibility = Visibility.Collapsed;
-                UICleaner.Visibility = Visibility.Collapsed;
-            }
-        }
-
-        private void UINumberButton_Click(object sender, RoutedEventArgs e)
-        {
-            double lNumber;
-            double.TryParse((e.Source as Button).Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out lNumber);
-
-            if (UIResultLabel.Content.ToString() == "0" || mSpecialSymbols.Contains(UIResultLabel.Content.ToString()) || mIsResult)
-            {
-                UIResultLabel.Content = lNumber;
-                mIsResult = false;
-            }
-            else
-            {
-                UIResultLabel.Content = $"{UIResultLabel.Content}{lNumber}";
-            }
-
-            mDisplayHistory.AppendHistoryFormula(lNumber.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
-        }
-
-        private void UIPointButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (!UIResultLabel.Content.ToString().Replace(',', '.').Contains('.'))
-            {
-                UIResultLabel.Content = $"{UIResultLabel.Content}.";
-                mDisplayHistory.AppendHistoryFormula((e.Source as Button).Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
-                mIsResult = false;
-            }
-        }
-
-        private void UIACButton_Click(object sender, RoutedEventArgs e)
-        {
-            UIResultLabel.Content = "0";
-            mDisplayHistory.AddNewHistory();
-            mLastNumber = 0;
-            mDisplayHistory.AppendHistoryFormula(string.Empty, UIHistoryTextBox);
-        }
-
-        private decimal TransformCoef(string pButtonPressed)
-        {
-            switch (pButtonPressed)
-            {
-                case "+/1":
-                    return -1;
-
-                case "%":
-                    return 0.01m;
-            }
-
-            return default;
-        }
-
-        private void UIMultiplyCoefButton_Click(object sender, RoutedEventArgs e)
-        {
-            decimal lTransformCoef = TransformCoef((e.Source as Button).Content.ToString());
-            decimal lResult;
-
-            if (decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out lResult))
-            {
-                if (!mIsResult)
-                {
-                    mDisplayHistory.RemoveHistoryFormula(UIResultLabel.Content.ToString().Length);
-                    UIResultLabel.Content = lResult * lTransformCoef;
-                }
-                else
-                {
-                    mIsResult = false;
-                    UIResultLabel.Content = mLastNumber * lTransformCoef;
-                }
-
-                mDisplayHistory.AppendHistoryFormula(UIResultLabel.Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
-            }
-        }
-
-        private void UIOperatorButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (!mSpecialSymbols.Contains(UIResultLabel.Content.ToString().LastOrDefault()))
-            {
-                mDisplayHistory.AppendHistoryFormula((e.Source as Button).Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
-
-                decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out mLastNumber);
-                UIResultLabel.Content = (e.Source as Button).Content.ToString();
-
-                if (sender == UIPlusButton)
-                {
-                    mSelectedOperator = SelectedOperator.Addition;
-                }
-                else if (sender == UIMinusButton)
-                {
-                    mSelectedOperator = SelectedOperator.Substraction;
-                }
-                else if (sender == UIMultiplyButton)
-                {
-                    mSelectedOperator = SelectedOperator.Multiplication;
-                }
-                else if (sender == UIDivideButton)
-                {
-                    mSelectedOperator = SelectedOperator.Division;
-                }
-            }
-        }
-
-        private void UIEqualButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (!mIsResult)
-            {
-                decimal lNewNumber;
-                if (decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out lNewNumber))
-                {
-                    switch (mSelectedOperator)
-                    {
-                        case SelectedOperator.Addition:
-                            UIResultLabel.Content = MathCompute.Add(mLastNumber, lNewNumber);
-                            break;
-                        case SelectedOperator.Substraction:
-                            UIResultLabel.Content = MathCompute.Substract(mLastNumber, lNewNumber);
-                            break;
-                        case SelectedOperator.Multiplication:
-                            UIResultLabel.Content = MathCompute.Multiply(mLastNumber, lNewNumber);
-                            break;
-                        case SelectedOperator.Division:
-                            UIResultLabel.Content = MathCompute.Divide(mLastNumber, lNewNumber);
-                            break;
-                    }
-
-                    mDisplayHistory.AppendHistoryFormula((e.Source as Button).Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
-                    mIsResult = true;
-                    mDisplayHistory.AddNewHistory();
-                    decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out mLastNumber);
-                    mDisplayHistory.AppendHistoryResult(UIResultLabel.Content.ToString(), UIHistoryTextBox);
-                    mDisplayHistory.AddNewHistory();
-                }
-            }
-        }
-
-        private void UIHistoryTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            UIHistoryScrollViewer.ScrollToEnd();
-        }
-
-        private void UICleaner_Click(object sender, RoutedEventArgs e)
-        {
-            mDisplayHistory.CleanHistory(ref UIHistoryTextBox);
-        }
-    }
-
-    public enum SelectedOperator
-    {
-        Addition,
-        Substraction,
-        Multiplication,
-        Division,
-        None
     }
 }
--- Udemy_Calculator/StandardCalculator.xaml.cs
+++ Udemy_Calculator/StandardCalculator.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,197 @@ namespace Udemy_Calculator
     /// </summary>
     public partial class StandardCalculator : UserControl
     {
+        private decimal mLastNumber;
+        private SelectedOperator mSelectedOperator = SelectedOperator.None;
+        private string mSpecialSymbols = "×÷+-";
+        private bool mIsResult = false;
+        private DisplayHistory mDisplayHistory;
+        private static double mHistoryWidth = 200d;
+        private const double mMainWindowWidth = 280d;
+        private const double mMainWindowHeight = 390d;
+        private GridLength mExpandedWidth = new GridLength(1, GridUnitType.Star);
+
         public StandardCalculator()
         {
             InitializeComponent();
+            mDisplayHistory = new DisplayHistory();
         }
+
+        private void UINumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            double lNumber;
+            double.TryParse((e.Source as Button).Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out lNumber);
+
+            if (UIResultLabel.Content.ToString() == "0" || mSpecialSymbols.Contains(UIResultLabel.Content.ToString()) || mIsResult)
+            {
+                UIResultLabel.Content = lNumber;
+                mIsResult = false;
+            }
+            else
+            {
+                UIResultLabel.Content = $"{UIResultLabel.Content}{lNumber}";
+            }
+
+            mDisplayHistory.AppendHistoryFormula(lNumber.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
+        }
+
+        private void UIPointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!UIResultLabel.Content.ToString().Replace(',', '.').Contains('.'))
+            {
+                UIResultLabel.Content = $"{UIResultLabel.Content}.";
+                mDisplayHistory.AppendHistoryFormula((e.Source as Button).Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
+                mIsResult = false;
+            }
+        }
+
+        private void UIACButton_Click(object sender, RoutedEventArgs e)
+        {
+            UIResultLabel.Content = "0";
+            mDisplayHistory.AddNewHistory();
+            mLastNumber = 0;
+            mDisplayHistory.AppendHistoryFormula(string.Empty, UIHistoryTextBox);
+        }
+
+        private decimal TransformCoef(string pButtonPressed)
+        {
+            switch (pButtonPressed)
+            {
+                case "+/1":
+                    return -1;
+
+                case "%":
+                    return 0.01m;
+            }
+
+            return default;
+        }
+
+        private void UIMultiplyCoefButton_Click(object sender, RoutedEventArgs e)
+        {
+            decimal lTransformCoef = TransformCoef((e.Source as Button).Content.ToString());
+            decimal lResult;
+
+            if (decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out lResult))
+            {
+                if (!mIsResult)
+                {
+                    mDisplayHistory.RemoveHistoryFormula(UIResultLabel.Content.ToString().Length);
+                    UIResultLabel.Content = lResult * lTransformCoef;
+                }
+                else
+                {
+                    mIsResult = false;
+                    UIResultLabel.Content = mLastNumber * lTransformCoef;
+                }
+
+                mDisplayHistory.AppendHistoryFormula(UIResultLabel.Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
+            }
+        }
+
+        private void UIOperatorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!mSpecialSymbols.Contains(UIResultLabel.Content.ToString().LastOrDefault()))
+            {
+                mDisplayHistory.AppendHistoryFormula((e.Source as Button).Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
+
+                decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out mLastNumber);
+                UIResultLabel.Content = (e.Source as Button).Content.ToString();
+
+                if (sender == UIPlusButton)
+                {
+                    mSelectedOperator = SelectedOperator.Addition;
+                }
+                else if (sender == UIMinusButton)
+                {
+                    mSelectedOperator = SelectedOperator.Substraction;
+                }
+                else if (sender == UIMultiplyButton)
+                {
+                    mSelectedOperator = SelectedOperator.Multiplication;
+                }
+                else if (sender == UIDivideButton)
+                {
+                    mSelectedOperator = SelectedOperator.Division;
+                }
+            }
+        }
+
+        private void UIEqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!mIsResult)
+            {
+                decimal lNewNumber;
+                if (decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out lNewNumber))
+                {
+                    switch (mSelectedOperator)
+                    {
+                        case SelectedOperator.Addition:
+                            UIResultLabel.Content = MathCompute.Add(mLastNumber, lNewNumber);
+                            break;
+                        case SelectedOperator.Substraction:
+                            UIResultLabel.Content = MathCompute.Substract(mLastNumber, lNewNumber);
+                            break;
+                        case SelectedOperator.Multiplication:
+                            UIResultLabel.Content = MathCompute.Multiply(mLastNumber, lNewNumber);
+                            break;
+                        case SelectedOperator.Division:
+                            UIResultLabel.Content = MathCompute.Divide(mLastNumber, lNewNumber);
+                            break;
+                    }
+
+                    mDisplayHistory.AppendHistoryFormula((e.Source as Button).Content.ToString(), UIHistoryTextBox, mIsResult, mLastNumber);
+                    mIsResult = true;
+                    mDisplayHistory.AddNewHistory();
+                    decimal.TryParse(UIResultLabel.Content.ToString().Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out mLastNumber);
+                    mDisplayHistory.AppendHistoryResult(UIResultLabel.Content.ToString(), UIHistoryTextBox);
+                    mDisplayHistory.AddNewHistory();
+                }
+            }
+        }
+
+        private void UIHistoryExpander_Expanded(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Width = mMainWindowWidth + mHistoryWidth;
+            UIHistoryExpanderColumn.Width = mExpandedWidth;
+
+            if (HistoryGrid != null)
+            {
+                HistoryGrid.Visibility = Visibility.Visible;
+                UICleaner.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UIHistoryExpander_Collapsed(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Width = mMainWindowWidth;
+            mExpandedWidth = UIHistoryExpanderColumn.Width;
+            UIHistoryExpanderColumn.Width = GridLength.Auto;
+
+            if (HistoryGrid != null)
+            {
+                HistoryGrid.Visibility = Visibility.Collapsed;
+                UICleaner.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void UIHistoryTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UIHistoryScrollViewer.ScrollToEnd();
+        }
+
+        private void UICleaner_Click(object sender, RoutedEventArgs e)
+        {
+            mDisplayHistory.CleanHistory(ref UIHistoryTextBox);
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division,
+        None
     }
 }
