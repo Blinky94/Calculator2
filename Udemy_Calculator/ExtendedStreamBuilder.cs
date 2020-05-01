24dd9d81e17395f2c03bc5d23d2fade845f9b431
fcs <fredericcazesulfourt@yahoo.fr>
2020-05-02 01:12:09 +0200

    Ajouts de tests unitaires + Ajout methode d"extension pour StreamBuilder

---
+++ Udemy_Calculator/ExtendedStreamBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Calculator
+{
+    public static class ExtendedStreamBuilder
+    {
+        private static char[] items;
+
+        public static bool Contains(this StringBuilder pSb, char[] pTab)
+        {
+            bool lIsSbContains = false;
+
+            for (int i = 0; i < pTab.Length; i++)
+            {
+                if (pSb.ToString().Contains(pTab[i]))
+                {
+                    lIsSbContains = true;
+                }
+            }
+
+            return lIsSbContains;
+        }
+    }
+}
--- Udemy_Calculator_Tests/Arithmetics_Tests.cs
+++ Udemy_Calculator_Tests/Arithmetics_Tests.cs
@@ -13,47 +13,103 @@ namespace Udemy_Calculator_Tests
     [TestClass]
     public class Arithmetics_Tests
     {
-        private string mNewFormula = string.Empty;
 
-        #region Test on parenthesis
+        #region Test on Constructor PEMDAS
 
         [TestMethod]
-        public void ParenthesisEquivalence_BadNumberParenthesisOpenedAndClosed_ReturnsNotCompleted()
+        public void PEMDAS_Constructor_ComputeFormula_ReturnsStreamBuilder()
         {
             string lFormula = "4*((5/2)";
 
             PEMDAS lPEMDAS = new PEMDAS(lFormula);
 
-            bool lboolParenthesis = lPEMDAS.AreParenthesisAreEquivalentNumber();
+            Assert.AreEqual(lFormula, lPEMDAS.mSb.ToString());
+        }
+
+        #endregion
+
+        #region Test on parenthesis
+
+        [TestMethod]
+        public void PEMDAS_ParenthesisEquivalence_BadNumberParenthesisOpenedAndClosed_ReturnsNotCompleted()
+        {
+            string lFormula = "4*((5/2)";
+
+            PEMDAS lPEMDAS = new PEMDAS(lFormula);
+
+            bool lboolParenthesis = lPEMDAS.ParenthesisAreEquivalent();
             Assert.IsFalse(lboolParenthesis);
         }
 
         [TestMethod]
-        public void ParenthesisEquivalence_GoodNumberParenthesisOpenedAndClosed_ReturnsCompleted()
+        public void PEMDAS_ParenthesisEquivalence_GoodNumberParenthesisOpenedAndClosed_ReturnsCompleted()
         {
             string lFormula = "4*((5/2))";
 
             PEMDAS lPEMDAS = new PEMDAS(lFormula);
 
-            bool lboolParenthesis = lPEMDAS.AreParenthesisAreEquivalentNumber();
+            bool lboolParenthesis = lPEMDAS.ParenthesisAreEquivalent();
+
             Assert.IsTrue(lboolParenthesis);
+        }
+
+        [TestMethod]
+        public void PEMDAS_IsRealOpenedParenthesis_NotOpenedParenthesis_ReturnsTrue()
+        {
+            string lFormula = "(5+2)";
+
+            PEMDAS lPEMDAS = new PEMDAS(lFormula);
+
+            Assert.IsTrue(lPEMDAS.IsRealOpenedParenthesis(0)); // GOOD '('
+        }
+
+        [TestMethod]
+        public void PEMDAS_IsRealOpenedParenthesis_RealOpenedParenthesis_ReturnsTrue()
+        {
+            string lFormula = "4*((5/2))";
+
+            PEMDAS lPEMDAS = new PEMDAS(lFormula);
+
+            Assert.IsTrue(lPEMDAS.IsRealOpenedParenthesis(2)); // GOOD '('
+        }
+
+        [TestMethod]
+        public void PEMDAS_IsRealOpenedParenthesis_ClosedParenthesis_ReturnsFalse()
+        {
+            string lFormula = "4*((5/2))";
+
+            PEMDAS lPEMDAS = new PEMDAS(lFormula);
+
+            Assert.IsFalse(lPEMDAS.IsRealOpenedParenthesis(7)); // BAD ')'
+        }
+
+        [TestMethod]
+        public void PEMDAS_IsRealOpenedParenthesis_ParenthesisPreceedByExponentSymbol_ReturnsFalse()
+        {
+            string lFormula = "2^(5/3)+4*((5/2))";
+
+            PEMDAS lPEMDAS = new PEMDAS(lFormula);
+
+            Assert.IsFalse(lPEMDAS.IsRealOpenedParenthesis(2)); // BAD '^('
+        }
+
+        [TestMethod]
+        public void PENDAS_ComputeParenthesis_PutFormulaWithParenthesis_ReturnsChunkWithParenthesis()
+        {
+            string lFormula = "2^(5/3)+4*((5/2))";
+
+            PEMDAS lPEMDAS = new PEMDAS(lFormula);
+
+            lPEMDAS.ComputeParenthesis();
+
+            Assert.AreEqual("(5/2)", new string(lPEMDAS.mChunk));
         }
 
         #endregion
 
         #region Tests on exponents
 
-        [TestMethod]
-        public void ComplexeFormulaWithExponent_ReturnsResult()
-        {
-            Assert.Inconclusive();
-            string lFormula = "(5^(4/2))-2/3*((7.5-(3^2))*(3*(7-2)))";
 
-            mNewFormula = lFormula;
-            //var lResult = ResolveFormula(ref mNewFormula);
-           // Assert.IsTrue(lResult);
-           // Assert.AreEqual("25-2/3*((7.5-9)*(3*(7-2)))", lFormula);
-        }
 
         #endregion
     }
