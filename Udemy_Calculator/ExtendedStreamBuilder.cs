65a451a1ba8776e8aa3e52d91aadc69f86edbaa5
fcs <fredericcazesulfourt@yahoo.fr>
2020-05-16 16:27:46 +0200

    Ajout de 3 fonctions pour compter les occurences de char/arrayChar/String dans une SB

--- Udemy_Calculator/ExtendedStreamBuilder.cs
+++ Udemy_Calculator/ExtendedStreamBuilder.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Udemy_Calculator
 {
+
+    //    Regex               Meaning
+    //      .       Matches any single character.
+    //      ?       Matches the preceding element once or not at all.
+    //      +   	Matches the preceding element once or more times.
+    //      *   	Matches the preceding element zero or more times.
+    //      ^   	Matches the starting position within the string.
+    //      $   	Matches the ending position within the string.
+    //      |   	Alternation operator.
+    //    [abc]     Matches a or b, or c.
+    //    [a-c]     Range; matches a or b, or c.
+    //    [^abc]    Negation, matches everything except a, or b, or c.
+    //      \s      Matches white space character.
+    //      \w      Matches a word character; equivalent to[a - zA - Z_0 - 9]
+    //      \b      Is an anchor which matches at a position that is called a word boundary. It allows to search for whole words.
     public static class ExtendedStreamBuilder
     {
         /// <summary>
@@ -88,7 +104,7 @@ namespace Udemy_Calculator
             {
                 int lIndex = lStr.ToString().IndexOf(pStr);
 
-                if(lIndex != -1)
+                if (lIndex != -1)
                 {
                     lList.Add(lIndex);
                     lStr.Replace(pStr, new string('#', pStr.Length), lIndex, pStr.Length);
@@ -116,6 +132,59 @@ namespace Udemy_Calculator
             string lval = new string(mNewStr);
 
             return pSb.Replace(pSb.ToString(), lval);
+        }
+
+        /// <summary>
+        /// Count the number of occurences contains in the current SB with each char in the array parameter
+        /// Return the number of occurences
+        /// </summary>
+        /// <param name="pSb"></param>
+        /// <param name="pChArray"></param>
+        /// <returns></returns>
+        public static int CountChar(this StringBuilder pSb, char[] pChArray)
+        {
+            int lCount = 0;
+
+            foreach (var lChar in pChArray)
+            {
+                lCount += pSb.CountChar(lChar);
+            }
+
+            return lCount;
+        }
+
+        /// <summary>
+        /// Count the number of occurences contains in the current SB with specific char in parameter
+        /// Return the number of occurences
+        /// </summary>
+        /// <param name="pSb"></param>
+        /// <param name="pCh"></param>
+        /// <returns></returns>
+        public static int CountChar(this StringBuilder pSb, char pCh)
+        {
+            int lCount = 0;
+
+            foreach (var lChar in pSb.ToString())
+            {
+                if (lChar == pCh)
+                {
+                    lCount++;
+                }
+            }
+
+            return lCount;
+        }
+
+        /// <summary>
+        /// Count the number of occurences contains in the current SB with specific string in parameter
+        /// Return the number of occurences
+        /// </summary>
+        /// <param name="pSb"></param>
+        /// <param name="pCh"></param>
+        /// <returns></returns>
+        public static int CountOccurences(this StringBuilder pSb, string pStr)
+        {
+            return Regex.Matches(pSb.ToString(), pStr).Count;
         }
     }
 }
