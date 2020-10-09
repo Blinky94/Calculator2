722bbbe40c2d812ff2386f33ebda45c793f338a3
fcs <fredericcazesulfourt@yahoo.fr>
2020-10-09 15:56:30 +0200

    Correction

--- Udemy_Calculator/EnumConverter.cs
+++ Udemy_Calculator/EnumConverter.cs
@@ -5,8 +5,7 @@ namespace Udemy_Calculator
 {
     public class EnumConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter,
-                              System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             Enum enumValue = default;
             if (parameter is Type type)
@@ -16,8 +15,7 @@ namespace Udemy_Calculator
             return enumValue;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter,
-                                  System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             int returnValue = 0;
             if (parameter is Type type)
